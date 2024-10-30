9ecd95a8eda60216b8befec1d04aa9e8e1037822
Shahriar Haque <shahriarhaquerahat10@gmail.com>
2024-10-30 12:16:17 +0600

    revert

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,14 @@
-// import * as dotenv from 'dotenv';
-// dotenv.config();
-
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.enableCors({
-//     origin: '*',
-//   });
-//   await app.listen(process.env.PORT || 8080);
-// }
-// bootstrap();
-
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
   });
-  await app.init();
+  await app.listen(process.env.PORT || 8080);
 }
-
-bootstrap().catch(err => {
-  console.error('NestJS application failed to start:', err);
-});
-
-export default server;
+bootstrap();
