af9fd7c70216de9a9ea3f4418ecb19d01dfee9ab
Shahriar Haque <shahriarhaquerahat10@gmail.com>
2024-10-30 11:52:04 +0600

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,36 @@
+// import * as dotenv from 'dotenv';
+// dotenv.config();
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors({
+//     origin: '*',
+//   });
+//   await app.listen(process.env.PORT || 8080);
+// }
+// bootstrap();
+
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors({
     origin: '*',
   });
-  await app.listen(process.env.PORT || 8080);
+  await app.init();
 }
+
 bootstrap();
+
+export default server;
